79db37a3c71febf6d8164fbf958168f9093d0b41
Brigeman <yurgen.brigeman@mail.ru>
2024-07-23 06:22:50 +0200

    fix errors

--- data_collector/models.py
+++ data_collector/models.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine, Column, String, Integer
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URI = 'postgresql+psycopg2://your_db_user:your_db_password@localhost/real_estate_db' # change exapmle 
+DATABASE_URI = 'postgresql+psycopg2://your_db_user:your_db_password@localhost/real_estate_db' # TODO change example 
 Base = declarative_base()
 
 engine = create_engine(DATABASE_URI)
